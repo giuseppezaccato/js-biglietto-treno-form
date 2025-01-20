521bb6786f08adb08b9271b7da44c3aca3806c20
giuseppezaccato <giuseppezaccato@gmail.com>
2025-01-20 20:28:11 +0100

    END: milestones 2+3 (+bonus)

--- main.js
+++ main.js
@@ -1,26 +1,26 @@
 
 //task seleziono e inizializzo gli input
+
+//*-----elementi nel FORM
+//nome passeggero
+const identityField = document.getElementById('identity-field')
 // Età del passeggero
 let età = document.getElementById("age");
 // numero di chilometri da percorrere
 let km = document.getElementById("km");
-// bottone => output
-const btn = document.getElementById("btn");
-// span => scrittura risultato output
-const ticket = document.getElementById("ticket");
+// bottone => output su cui applicare eventlistener!
+const go = document.getElementById("go");
 
-////  - applicato uno sconto del 20% per i minorenni
-//// if (age < 18) {
-////     price -= price * 0.2;
-////     console.log(`in quanto minorenne costo biglietto =  ${price.toFixed(2)} € `);
-////      sconto del 40% per gli over 65.
-//// } else if (age >= 65) {
-////     price *= .6;
-////     console.log(`in quanto over 65 costo biglietto = ${price.toFixed(2)} €`);
-//// } else {
-////      prezzo tariffa standard
-////     console.log(`il prezzo del biglietto è di ${price.toFixed(2)} €`);
-//// }
+//*-----elementi nel biglietto in stampa!
+const biglietto = document.getElementById("biglietto")
+//nome passeggero
+const identity = document.getElementById('identity');
+//tariffa applicata
+const discount = document.getElementById('discount');
+// numero random corsa
+const idcorsa = document.getElementById('idcorsa')
+// output prezzo biglietto
+const ticket = document.getElementById("ticket");
 
 // task MILESTONE 1:
 // Iniziamo implementando il programma senza alcuna estetica: usando esclusivamente due input e un bottone (non stilizzati),
@@ -28,31 +28,45 @@ const ticket = document.getElementById("ticket");
 
 //task organizzo l'output con l'eventlistener contenente
 //*----- I STEP => RACCOLTA DATI selezionati sopra con l'input!
-//*----- II STEP =>  IF + INNERHTML(stampa) risultato in ticket!
-btn.addEventListener("click", function (event) {
+//*----- II STEP => (stampa) risultato nel biglietto d-none e cambia class in d-flex
+//*----- III STEP => IF + INNERHTML
+go.addEventListener("click", function (event) {
+
+    biglietto.classList.replace('d-none', 'd-flex');
+
+
+    event.preventDefault()
+
+    identity.innerHTML = identityField.value;
+    idcorsa.innerHTML += Math.floor(Math.random() * 9000) + 1;
+
     età = età.value;
     km = km.value;
     let price = km * 0.21; //? perchè il tofixed applicato qui non funziona?
-    event.preventDefault()
 
-        //* if ternario con meccanismo applicazione sconto e stampa prezzo DENTRO a ticket
-        (età < 18) ? (price -= price * 0.2, ticket.innerHTML = `il prezzo è ${price.toFixed(2)} €`)  //vedi value 1
-        : (età >= 65) ? (price *= .6, ticket.innerHTML = `il prezzo è ${price.toFixed(2)} €`)        //vedi value 3
-            : (price, ticket.innerHTML = `il prezzo è ${price.toFixed(2)} €`);
+    //* if ternario con meccanismo applicazione sconto e stampa prezzo DENTRO a ticket
+    (età == "minorenne") ? (price -= price * 0.2, discount.innerHTML = `Tariffa ${età}`, ticket.innerHTML += ` ${price.toFixed(2)} €`)
+        : (età == "over") ? (price *= .6, discount.innerHTML = `Tariffa ${età}`, ticket.innerHTML += `${price.toFixed(2)} €`)
+            : (discount.innerHTML = `Tariffa ${età}`, ticket.innerHTML += `${price.toFixed(2)} €`);
     console.log(ticket);
+
 });
+
+//-----BONUS-----
+function riavvolgi() {
+    location.reload();
+}
 
 //task MILESTONE 2:
 // realizzo un form in pagina in cui l’utente potrà inserire i dati e visualizzare il calcolo finale con il prezzo.
 // Il recap dei dati e l'output del prezzo finale, andranno quindi stampati in pagina
 // (il prezzo dovrà essere formattato con massimo due decimali, per indicare i centesimi sul prezzo)
-
-
+//*-----DONE-----
 
 //task MILESTONE 3:
-// Ora che la logica è funzionante in pagina, possiamo andare a dedicarci allo stile, raffinando la parte di HTML e CSS in modo da renderla esteticamente gradevole.
-
-
+// Ora che la logica è funzionante in pagina, possiamo andare a dedicarci allo stile,
+// raffinando la parte di HTML e CSS in modo da renderla esteticamente gradevole.
+//*-----DONE-----
 
 
 

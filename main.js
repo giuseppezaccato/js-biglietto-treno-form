344f08f62c51e2134d7b58f10fb0fb1f069f19a6
giuseppezaccato <giuseppezaccato@gmail.com>
2025-01-20 15:01:57 +0100

    FIX: milestone1

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 
-//task - Età del passeggero
-const age = document.getElementById("age");
-//task - numero di chilometri da percorrere
-const km = document.getElementById("km");
+//task seleziono e inizializzo gli input
+// Età del passeggero
+let età = document.getElementById("age");
+// numero di chilometri da percorrere
+let km = document.getElementById("km");
+// bottone => output
+const btn = document.getElementById("btn");
+// span => scrittura risultato output
+const ticket = document.getElementById("ticket");
+
 
 
 ////  - applicato uno sconto del 20% per i minorenni
@@ -22,27 +28,27 @@ const km = document.getElementById("km");
 // Iniziamo implementando il programma senza alcuna estetica: usando esclusivamente due input e un bottone (non stilizzati),
 //  realizziamo le specifiche scritte sopra. La risposta finale (o output) sarà anch’essa da scrivere in console.
 
-const btn = document.getElementById("btn");
-const ticket = document.getElementById("ticket");
-
+//task organizzo l'output con l'eventlistener contenente
+//*----- I STEP => RACCOLTA DATI selezionati sopra con l'input!
+//*----- II STEP =>  IF + INNERHTML(stampa) risultato in ticket!
 btn.addEventListener("click", () => {
-    let età = age.value;
-    let distance = km.value;
-    let price = distance * .21; //fixed! questo ve DENTRO per forza sennò distance non funziona!
+    età = età.value;
+    km = km.value;
+    let price = ((km * .21).toFixed(2)); // questo va DENTRO per forza sennò distance non funziona!, .toFixed(2)!
 
     //* if ternario con meccanismo applicazione sconto e stampa prezzo DENTRO a ticket
     (età < 18) ? (price -= price * 0.2, ticket.innerHTML = `il prezzo è ${price} €`)
         : (età >= 65) ? (price *= .6, ticket.innerHTML = `il prezzo è ${price} €`)
             : (price, ticket.innerHTML = `il prezzo è ${price} €`);
     return console.log(ticket);
-}
-)
+});
 
 
 
 //task MILESTONE 2:
 // Solo una volta che il milestone 1 sarà completo e funzionante allora realizzeremo un form in pagina in cui l’utente potrà inserire i dati e visualizzare il calcolo finale con il prezzo.
-// Il recap dei dati e l'output del prezzo finale, andranno quindi stampati in pagina (il prezzo dovrà essere formattato con massimo due decimali, per indicare i centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+// Il recap dei dati e l'output del prezzo finale, andranno quindi stampati in pagina (il prezzo dovrà essere formattato con massimo due decimali, per indicare i centesimi sul prezzo).
+// Questo richiederà un minimo di ricerca.
 
 //task MILESTONE 3:
 // Ora che la logica è funzionante in pagina, possiamo andare a dedicarci allo stile, raffinando la parte di HTML e CSS in modo da renderla esteticamente gradevole.

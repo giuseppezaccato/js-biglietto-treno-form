fa0e25717f131648052e8fb2cf51b51e6167eae0
giuseppezaccato <giuseppezaccato@gmail.com>
2025-01-20 14:29:36 +0100

    MILESTONE1

--- main.js
+++ main.js
@@ -1,59 +1,51 @@
-//----------TASK 1----------
-//------RACCOLTA DATI DALL'UTENTE
-//  -km richiesti per il viaggio 
-//  -età del passeggero
 
-// ricorda sempre di fare in modo che i dati raccolti vengano trasformati in numeri,
-//  o almeno fai un controllo in modo che lo diventino (parseInt)
-
-const age = parseInt(prompt("Quanti anni hai?", 35));
-const km = parseInt(prompt("inserisci quanti km intendi percorrere per sapere quanto costerà il tuo biglietto", 120));
-let price = km * 0.21;
+//task - Età del passeggero
+const age = document.getElementById("age");
+//task - numero di chilometri da percorrere
+const km = document.getElementById("km");
 
 
-//----------TASK 2----------
-//-----SCONTO APPLICATO IN BASE A ETà PASSEGGERO
-// IF < 18 = sconto 20%
-// ELSE IF >= 65 = sconto 40%
-// ELSE = prezzo pieno
-// ricorda in ogni caso di SOVRASCRIVERE il prezzo prima di fare il log,
-// richiamando la variabile, e non SOLO dandogliela nella condizione di IF
+////  - applicato uno sconto del 20% per i minorenni
+//// if (age < 18) {
+////     price -= price * 0.2;
+////     console.log(`in quanto minorenne costo biglietto =  ${price.toFixed(2)} € `);
+////      sconto del 40% per gli over 65.
+//// } else if (age >= 65) {
+////     price *= .6;
+////     console.log(`in quanto over 65 costo biglietto = ${price.toFixed(2)} €`);
+//// } else {
+////      prezzo tariffa standard
+////     console.log(`il prezzo del biglietto è di ${price.toFixed(2)} €`);
+//// }
 
+// task MILESTONE 1:
+// Iniziamo implementando il programma senza alcuna estetica: usando esclusivamente due input e un bottone (non stilizzati),
+//  realizziamo le specifiche scritte sopra. La risposta finale (o output) sarà anch’essa da scrivere in console.
 
-// ------------------------------JS Assignment Operators----------------------------------------
-//                          https://www.w3schools.com/js/js_operators.asp
-// l'equivalente semplificato del calcolo sotto si può riassumere con le seguenti espressioni:
-// price -= price * 0.2(sè stesso - price * .2);
-// o ancora meglio:
-// price = (price * 1) - (price * .2)=> price * (1 - .2)=> price * .8;
-// price *= .8(sè stesso moltiplicato 0.8);
-// ---------------------------------------------------------------------------------------------
+const btn = document.getElementById("btn");
+const ticket = document.getElementById("ticket");
 
+btn.addEventListener("click", () => {
+    let età = age.value;
+    let distance = km.value;
+    let price = distance * .21; //fixed! questo ve DENTRO per forza sennò distance non funziona!
 
-if (age < 18) {
-    price -= price * 0.2;
-    console.log(`in quanto minorenne costo biglietto =  ${price.toFixed(2)} € `);
-    alert("il prezzo del biglietto è di" + " " + price.toFixed(2) + " €");
-
-} else if (age >= 65) {
-    // per il discorso fatto sopra a riga 29
-    price *= .6;
-    console.log(`in quanto over 65 costo biglietto = ${price.toFixed(2)} €`);
-    alert("il prezzo del biglietto è di" + " " + price.toFixed(2) + " €");
-
-} else {
-    /* qui non c'è bisogno di dichiarare nulla => dovrebbe prendere il price non riassegnato,
-    quindi il prezzo pieno per intenderci */
-    console.log(`il prezzo del biglietto è di ${price.toFixed(2)} €`);
-    alert("il prezzo del biglietto è di" + " " + price.toFixed(2) + " €");
+    //* if ternario con meccanismo applicazione sconto e stampa prezzo DENTRO a ticket
+    (età < 18) ? (price -= price * 0.2, ticket.innerHTML = `il prezzo è ${price} €`)
+        : (età >= 65) ? (price *= .6, ticket.innerHTML = `il prezzo è ${price} €`)
+            : (price, ticket.innerHTML = `il prezzo è ${price} €`);
+    return console.log(ticket);
 }
+)
 
-// a questopunto, avendo dato il prezzo biglietto e dichiarato sopra,
-// per evitare di ripetere il console.log 3 volte in ogni if, lo si puo fare
-// una sola volta qui sotto comprendendo nella stampa il .toFixed(2)
 
-// console.log(`il prezzo del biglietto è di ${price.toFixed(2)} €`);
-// alert("il prezzo del biglietto è di" + " " + price.toFixed(2) + " €");
+
+//task MILESTONE 2:
+// Solo una volta che il milestone 1 sarà completo e funzionante allora realizzeremo un form in pagina in cui l’utente potrà inserire i dati e visualizzare il calcolo finale con il prezzo.
+// Il recap dei dati e l'output del prezzo finale, andranno quindi stampati in pagina (il prezzo dovrà essere formattato con massimo due decimali, per indicare i centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+
+//task MILESTONE 3:
+// Ora che la logica è funzionante in pagina, possiamo andare a dedicarci allo stile, raffinando la parte di HTML e CSS in modo da renderla esteticamente gradevole.
 
 
 
